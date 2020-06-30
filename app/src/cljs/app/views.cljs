55c5303e6d2d45ded7b9d0118af0e85fc0599767
Christobal <geekarist@users.noreply.github.com>
2020-06-30 08:32:44 +0200

    Enclose each panel in a div

--- app/src/cljs/app/views.cljs
+++ app/src/cljs/app/views.cljs
@@ -5,18 +5,18 @@
     [app.events :as ev]))
 
 (defn home-panel []
-  [:button
-   {:on-click #(re-frame/dispatch [::ev/navigate-to :panel/start-end-selection])}
-   "Where to?"])
+  [:div
+   [:button
+    {:on-click #(re-frame/dispatch [::ev/navigate-to :panel/start-end-selection])}
+    "Where to?"]])
 
 (defn start-end-selection-panel []
-  [:p "Please select your start and end points"])
+  [:div
+   [:p "Please select your start and end points"]])
 
 (defn main-panel []
   (let [active-panel (re-frame/subscribe [::subs/active-panel])]
     (fn []
-      [:div
-       ((condp = @active-panel
-          :panel/home home-panel
-          :panel/start-end-selection start-end-selection-panel))
-       ])))
+      (condp = @active-panel
+        :panel/home (home-panel)
+        :panel/start-end-selection (start-end-selection-panel)))))
