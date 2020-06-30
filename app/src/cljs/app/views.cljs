6571492da075298b8498a405e3970c82041b647b
Christobal <geekarist@users.noreply.github.com>
2020-06-30 08:39:49 +0200

    Implement start/end selection panel view

--- app/src/cljs/app/views.cljs
+++ app/src/cljs/app/views.cljs
@@ -1,21 +1,27 @@
 (ns app.views
   (:require
-    [re-frame.core :as re-frame]
+    [rf.core :as rf]
     [app.subs :as subs]
     [app.events :as ev]))
 
 (defn home-panel []
   [:div
    [:button
-    {:on-click #(re-frame/dispatch [::ev/navigate-to :panel/start-end-selection])}
+    {:on-click #(rf/dispatch [::ev/navigate-to :panel/start-end-selection])}
     "Where to?"]])
 
 (defn start-end-selection-panel []
   [:div
-   [:p "Please select your start and end points"]])
+   [:p [:input {:type        "text"
+                :placeholder "Start"
+                :on-change   #(rf/dispatch [::ev/set-journey-start (.-value %)])}]]
+   [:p [:input {:type        "text"
+                :placeholder "End"
+                :on-change   #(rf/dispatch [::ev/set-journey-end (.-value %)])}]]
+   [:p [:button {:on-click #(rf/dispatch [::ev/get-journeys])} "Go!"]]])
 
 (defn main-panel []
-  (let [active-panel (re-frame/subscribe [::subs/active-panel])]
+  (let [active-panel (rf/subscribe [::subs/active-panel])]
     (fn []
       (condp = @active-panel
         :panel/home (home-panel)
