2f1080563a885e47f99ad12fe0fe22f3858adc27
Christobal <geekarist@users.noreply.github.com>
2020-09-08 08:47:37 +0200

    Send request by HTTP using cljs-ajax

--- app/src/cljs/app/effects.cljs
+++ app/src/cljs/app/effects.cljs
@@ -1,10 +1,15 @@
 (ns app.effects
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [ajax.core :refer [GET]]))
+
+(defn call [req]
+  (GET (:url req)
+       {:params (:params req)}))
 
 (re-frame/reg-fx
   ::get-suggestions
   (fn [request]
-    (if request (println request))))
+    (if request (call request))))
 
 (re-frame/reg-cofx
   ::get-current-time
