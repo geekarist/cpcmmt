55be28b6ea1a346bfe17921d0b02be94c26066af
Christobal <geekarist@users.noreply.github.com>
2020-09-14 08:00:34 +0200

    Mock `/places` using a file in public resources

--- app/src/cljs/app/config.cljs
+++ app/src/cljs/app/config.cljs
@@ -3,4 +3,5 @@
 (def debug?
   ^boolean goog.DEBUG)
 
-(def navitia-base-url "https://api.navitia.io")
+;(def navitia-base-url "https://api.navitia.io")
+(def navitia-base-url "/mock/api.navitia.io")
