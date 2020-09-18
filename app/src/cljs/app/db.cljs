5031463c38b58d20a13c460b2c43057da2d86c3f
Christobal <geekarist@users.noreply.github.com>
2020-09-18 08:10:41 +0200

    Don't specify default values to nil

--- app/src/cljs/app/db.cljs
+++ app/src/cljs/app/db.cljs
@@ -2,8 +2,5 @@
 
 (def default-db
   {::active-panel        ::panel-home
-   ::journey-start       "a"
-   ::journey-end         "b"
    ::journeys            []
-   ::autosuggest-error   nil
    ::autosuggest-results []})
