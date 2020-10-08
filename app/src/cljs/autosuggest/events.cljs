57de11a2a8f251d627080f5bc5e1fa06e052661d
Christobal <geekarist@users.noreply.github.com>
2020-10-08 08:30:13 +0200

    Extract debounce-delay-expired?

--- app/src/cljs/autosuggest/events.cljs
+++ app/src/cljs/autosuggest/events.cljs
@@ -17,19 +17,22 @@
 
 (def autosuggest-debounce-delay-ms 1000)
 
+(defn debounce-delay-expired? [db current-time-ms]
+  (>= current-time-ms
+      (+ (::db/autosuggest-last-query-time-ms db)
+         autosuggest-debounce-delay-ms)))
+
 (re-frame/reg-event-fx
   ::autosuggest-query-confirmation
   [(re-frame/inject-cofx ::ef/get-current-time)]
   (fn [{db              :db
         current-time-ms ::ef/current-time-ms}
-       [_ value]]
-    (if (and (not= value "")
-             (>= current-time-ms
-                 (+ (::db/autosuggest-last-query-time-ms db)
-                    autosuggest-debounce-delay-ms)))
+       [_ query]]
+    (if (and (debounce-delay-expired? db current-time-ms)
+             (not= query ""))
       {::ef/get-anything
        {:url        (gstr/format (str conf/navitia-base-url "/v1/coverage/%s/places") conf/navitia-coverage)
-        :params     {:q               value
+        :params     {:q               query
                      :key             sec/navitia-api-key
                      :disable_geojson "true"}
         :on-success ::suggestions-resp-received
