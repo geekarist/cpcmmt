04657ab088ca999cf188c4c2ae0700fe190e5eb3
Christobal <geekarist@users.noreply.github.com>
2020-10-15 08:36:00 +0200

    Fix query suppression using backspace

--- app/src/cljs/autosuggest/views.cljs
+++ app/src/cljs/autosuggest/views.cljs
@@ -9,19 +9,25 @@
                    (rf/dispatch [::ev/reset text]))}
    text])
 
+(defn assoc-unless [coll k v cond?]
+  (if cond?
+    coll
+    (assoc coll k v)))
+
 (defn autosuggest-component [initial-query on-suggestion-selected]
   (let [updated-query @(rf/subscribe [::subs/query])
+        query-updated? (some? updated-query)
+        query-field-value (if updated-query updated-query
+                                            (do (rf/dispatch [::ev/autosuggest-query-change initial-query])
+                                                initial-query))
         error @(rf/subscribe [::subs/autosuggest-error])
         results @(rf/subscribe [::subs/autosuggest-results])]
     [:div.container.mt-3
      [:div.form-group
       [:input#autosuggest-query-field.form-control
        (-> {:type      "text"
-            :on-change #(rf/dispatch [::ev/autosuggest-query-change (-> % .-target .-value)])
-            :value     (if updated-query
-                         updated-query
-                         (do (rf/dispatch [::ev/autosuggest-query-change initial-query])
-                             initial-query))})]]
+            :on-change #(rf/dispatch [::ev/autosuggest-query-change (-> % .-target .-value)])}
+           (assoc-unless :value query-field-value query-updated?))]]
      (if error
        [:div.alert.alert-warning "Error fetching suggestions: " error]
        (->> results
